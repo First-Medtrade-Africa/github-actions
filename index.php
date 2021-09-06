d04b76a6d251a560001d4fa0b41823de2071b636
Babajide Tomoshegbo <babajidetomoshegbo@yahoo.com>
2021-09-06 15:34:12 +0100

    added p tag

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Staging website</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque a itaque incidunt labore consequuntur odio qui, architecto ut ducimus. Ab, dignissimos. Nisi neque vero doloremque dignissimos repudiandae accusantium veniam totam.</p>
 </body>
 </html>
