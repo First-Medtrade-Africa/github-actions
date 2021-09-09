aeb777969c0ce25adf4a3d2939b6ab189b8b5ce3
Babajide Tomoshegbo <babajidetomoshegbo@yahoo.com>
2021-09-09 16:30:53 +0100

    added core files

---
+++ core/init.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+$GLOBALS['config'] = array(
+    'mysql' => array(
+        'host' => 'localhost',
+        'username' => 'firstmf1_Ubyjude',
+        'password' => '6yY]?*_Nem{p',
+        'db' => 'firstmf1_FMTA'
+    ),
+);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+require_once 'functions/sanitize.php';
+
+
